35b16f867de6e0617e77a14b6dea62a5edccbd11
izotarina <irunchikt@mail.ru>
2021-11-27 02:29:30 +0300

    Scripted auto-commit on change (2021-11-27 02:29:30) by gitwatch.sh

--- src/main/java/lab3/AirportApp.java
+++ src/main/java/lab3/AirportApp.java
@@ -4,6 +4,9 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportApp {
-    SparkConf conf = new SparkConf().setAppName("lab3");
-    JavaSparkContext sc = new JavaSparkContext(conf);
+    public static void main() {
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+    }
+
 }
