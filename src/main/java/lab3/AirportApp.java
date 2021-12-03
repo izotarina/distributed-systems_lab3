ad3a531252cd32d5a4d5b87203b8d6b5debf115d
izotarina <irunchikt@mail.ru>
2021-12-04 02:37:02 +0300

    Scripted auto-commit on change (2021-12-04 02:37:02) by gitwatch.sh

--- src/main/java/lab3/AirportApp.java
+++ src/main/java/lab3/AirportApp.java
@@ -4,13 +4,14 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import java.util.Arrays;
+
 public class AirportApp {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> airportsFile = sc.textFile("L_AIRPORT_ID.csv");
-        JavaRDD<String> splitted = airportsFile.flatMap(s -> Arrays.stream(s.split("\n")).iterator()
-        );
+        JavaRDD<String> airportsRows = airportsFile.flatMap(s -> Arrays.stream(s.split("\n")).iterator();
     }
 }
