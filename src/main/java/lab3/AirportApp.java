1b4c0ef73aeb598c7476a8e0888da62527d1b625
izotarina <irunchikt@mail.ru>
2021-12-04 02:53:00 +0300

    Scripted auto-commit on change (2021-12-04 02:53:00) by gitwatch.sh

--- src/main/java/lab3/AirportApp.java
+++ src/main/java/lab3/AirportApp.java
@@ -14,14 +14,14 @@ public class AirportApp {
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> airportsFile = sc.textFile("L_AIRPORT_ID.csv");
-        JavaPairRDD<String, Long> airportsMap =
+        JavaPairRDD<String, String> airportsMap =
             airportsFile.mapToPair(
                 s -> {
                     String[] columns = s.split(",");
-                    return new Tuple2<>(columns[0], columns[1])
+                    return new Tuple2<>(columns[0], columns[1]);
                 }
             );
 
-        JavaRDD<String> flightsFile = sc.textFile("L_AIRPORT_ID.csv");
+        JavaRDD<String> flightsFile = sc.textFile("664600583_T_ONTIME_sample.csv");
     }
 }
