3f66175a9e4079a2913c5ea896f9e70c6115d888
izotarina <irunchikt@mail.ru>
2021-12-04 02:33:57 +0300

    Scripted auto-commit on change (2021-12-04 02:33:57) by gitwatch.sh

--- src/main/java/lab3/AirportApp.java
+++ src/main/java/lab3/AirportApp.java
@@ -1,12 +1,14 @@
 package lab3;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportApp {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
-    }
 
+        JavaRDD<String> airpoertFile = sc.textFile(Hadoop "war-and-peace-1.txt");
+    }
 }
