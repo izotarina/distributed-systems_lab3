ddcfa8a073547058cfb31c83585c57745de5f144
izotarina <irunchikt@mail.ru>
2021-12-04 03:25:14 +0300

    Scripted auto-commit on change (2021-12-04 03:25:14) by gitwatch.sh

--- src/main/java/lab3/AirportStatisticSerializable.java
+++ src/main/java/lab3/AirportStatisticSerializable.java
@@ -3,6 +3,19 @@ package lab3;
 import java.io.Serializable;
 
 public class AirportStatisticSerializable implements Serializable {
+    private double maxDelay;
+    private double cancelledAndDelayedFlightsPart;
 
-    public AirportStatisticSerializable(double maxDelay, cancelledAndDela)
+    public AirportStatisticSerializable(double maxDelay, double cancelledAndDelayedFlightsPart) {
+        this.maxDelay = maxDelay;
+        this.cancelledAndDelayedFlightsPart = cancelledAndDelayedFlightsPart;
+    }
+
+    public double getCancelledAndDelayedFlightsPart() {
+        return cancelledAndDelayedFlightsPart;
+    }
+
+    public double getMaxDelay() {
+        return maxDelay;
+    }
 }
